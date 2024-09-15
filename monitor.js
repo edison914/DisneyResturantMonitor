ad65114189ffff25603670ed9d2693bf61a1a17d
Zhongyuan Ding <zhongyuan.d@gmail.com>
2024-09-14 21:34:19 -0400

    removed token

--- monitor.js
+++ monitor.js
@@ -82,7 +82,6 @@ const fetchTimeSlot = async () => {
   //       "User-Agent":
   //         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
   //       Accept: "application/json, text/plain, */*",
-  //       Authorization: "BEARER fd04beda58d34f398637016428c5022",
   //       Cookie: cookieString,
   //     },
   //   });
